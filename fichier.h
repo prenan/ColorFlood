d1599dffb77bc371c90fc0201f97fd4d863baee0
Renan <renan.pincemin@outlook.fr>
2017-02-13 14:21:33 +0100

    trad en ang

--- fichier.h
+++ fichier.h
@@ -3,9 +3,9 @@
 
 #include "grille.h"
 
-FILE* ouvre(char const* nom_fichier) ;
-grille init_file(int size, char* nom_fichier);
-void ferme(FILE* fichier);
-int fin_de_fichier(FILE* fichier);
+FILE* open_file(char const* file_name);
+grille init_file(int size, char* file_name);
+void close_file(FILE* file);
+int end_of_file(FILE* file);
 
 #endif
