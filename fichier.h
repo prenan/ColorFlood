d35b9c9f8c1dd4ac68bf49218376da190de490e1
prenan <renan.pincemin@outlook.fr>
2017-02-20 17:45:40 +0100

    suite doxygen fichier.h

--- fichier.h
+++ fichier.h
@@ -24,7 +24,23 @@
  * \return -1 si erreur à l'ouverture, autre valeur sinon.
  */
 int open_file(char const* file_name);
+
+/**
+ * \fn void close_file(int file_in)
+ * \brief Fonction de fermeture d'un fichier.
+ *
+ * \param file_in Fichier à fermer.
+ * \return ??.
+ */
 void close_file(int file_in);
+
+/**
+ * \fn int size_file(char* file_name)
+ * \brief Fonction qui détermine la taille du jeu.
+ *
+ * \param file_name Nom du fichier avec son extension.
+ * \return -1 si erreur dans le contenu du fichier, la valeur de la taille sinon.
+ */
 int size_file(char* file_name); /*check files content and return game size if ok*/
 grille init_file(int size, char* file_name);
 void export_file(grille plateau, int size);
