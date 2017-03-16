13e10622412b73c9d1fead2aea711777cd2063ba
HaoLIU94 <liuhaoxdu@gmail.com>
2017-03-16 18:43:50 +0100

    Add the N_tree structure and corresponding fontions to try to build a solveur

---
+++ n_arbres.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <math.h>
+#include <stdlib.h>
+#include "n_arbres.h"
+
+typedef struct t_narbre NNoeud,*NArbre;
+
+int narbreVide(NArbre a);
+
+NArbre nouvelNArbre(char c);
+
+/**
+ ** @brief create a new node with initial color c
+ **/
+
+NArbre ajoutFils(NArbre a,NArbre fils);
+
+void affNArbreRec(NArbre a, int num);
+
+void node_delete(NArbre n);
+
+/**
+ **  detect if n has children
+ **/
+
+bool node_isinner(NArbre n);
+
+/**
+ ** detect if @a n is a leaf
+ **/
+
+bool node_isleaf(NArbre n);
+
+
+void tree_delete(NArbre n);
---
+++ n_arbres.h
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define NARBREVIDE NULL
+#define MAXFILS 5
+
+int max(int a,int b) {
+	  return(a>b ? a:b);
+} 
+
+typedef struct t_narbre{
+      char c;       /*the current color*/
+      struct t_narbre *tabfils[MAXFILS];
+      int nbFils;
+}NNoeud,*NArbre;
+
+int narbreVide(NArbre a){
+    return (a==NARBREVIDE);
+}
+
+NArbre nouvelNArbre(char c){
+    NNoeud *n;
+    n=(NNoeud*)malloc(sizeof(NNoeud));
+    n->c=c;
+    for(int i=0;i<MAXFILS;i++)
+       n->tabfils[i]=NULL;
+    n->nbFils=0;
+    return n;
+}
+
+/**
+ ** @brief create a new node with initial color c
+ **/
+
+NArbre ajoutFils(NArbre a,NArbre fils){
+  
+    for(int i=0;i<MAXFILS;i++){
+         if(a->tabfils[i]==NULL){
+            a->tabfils[i]=fils;
+            break;
+           }
+    }
+    a->nbFils++;
+    return a;
+}
+
+void affNArbreRec(NArbre a, int num){
+     if (!narbreVide(a)) {
+   //afficher les infos du noeud *a
+     	printf("n%d [label=%c];\n",num,a->c);
+     	if(num!=0)
+	     printf("n%d -> n%c;\n",(num-1)/MAXFILS,num);
+	for(int i=0;i<a->nbFils;i++)
+     	     affNArbreRec(a->tabfils[i],MAXFILS*num+i+1);
+    }
+}
+
+void node_delete(NArbre n) {
+	  if (n) {
+	  	    int i;
+	  	    for(i=0;i<MAXFILS;i++)
+		      assert(!n->tabfils[i]);
+	  };
+	  free(n);
+}
+
+/**
+ **  detect if n has children
+ **/
+
+bool node_isinner(NArbre n) {
+	  assert(n);
+	  int i;
+	  for(i=0;i<MAXFILS;i++)
+      {   
+         if(n->tabfils[i])
+         	return true;
+      }
+      return false;
+	  
+}
+
+/**
+ ** detect if @a n is a leaf
+ **/
+
+bool node_isleaf(NArbre n) {
+	  assert(n);
+	  return !node_isinner(n);
+}
+
+
+void tree_delete(NArbre n) {
+	  if (n) {
+	  	int i;
+	  	for (i = 0; i < MAXFILS; ++i)
+	  	{
+	  		tree_delete(n->tabfils[i]);
+	  	}
+	  	for (i = 0; i < MAXFILS; ++i)
+	  	{
+	  		n->tabfils[i]=0;
+	  	}
+		    node_delete(n);
+	  }
+}
