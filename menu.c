e8fc5773be7eb26a0898a5f2486d30cbc9ab5c6a
prenan <renan.pincemin@outlook.fr>
2017-05-12 19:35:06 +0200

    up valgrind menu ok

--- menu.c
+++ menu.c
@@ -1,8 +1,9 @@
 #include "menu.h"
 
 
-void display_niveau(int niveau, TTF_Font *police_petite, SDL_Surface *ecran, SDL_Surface **facile, SDL_Surface **normal, SDL_Surface **expert, SDL_Color couleur_texte_W, SDL_Color couleur_texte_G)		
+void display_niveau(int niveau, TTF_Font *police_petite, SDL_Surface *ecran, SDL_Color couleur_texte_W, SDL_Color couleur_texte_G)		
 {
+	SDL_Surface *facile, *normal, *expert;
 	RGB G = {51, 51, 51};
 	RGB W = {255, 255, 255};
 	if (niveau == 1)	// niveau facile
@@ -13,9 +14,9 @@ void display_niveau(int niveau, TTF_Font *police_petite, SDL_Surface *ecran, SDL
 		drawSquare(ecran, 220, 264, 44, W);
 		drawSquare(ecran, 308, 264, 44, W);
 		drawSquare(ecran, 352, 264, 44, W);
-		*facile = TTF_RenderUTF8_Blended(police_petite, "Facile", couleur_texte_W); 
-		*normal = TTF_RenderUTF8_Blended(police_petite, "Normal", couleur_texte_G); 
-		*expert = TTF_RenderUTF8_Blended(police_petite, "Expert", couleur_texte_G); 
+		facile = TTF_RenderUTF8_Blended(police_petite, "Facile", couleur_texte_W); 
+		normal = TTF_RenderUTF8_Blended(police_petite, "Normal", couleur_texte_G); 
+		expert = TTF_RenderUTF8_Blended(police_petite, "Expert", couleur_texte_G); 
 	}
 	if (niveau == 2)	// niveau normal
 	{
@@ -25,9 +26,9 @@ void display_niveau(int niveau, TTF_Font *police_petite, SDL_Surface *ecran, SDL
 		drawSquare(ecran, 220, 264, 44, G);
 		drawSquare(ecran, 308, 264, 44, W);
 		drawSquare(ecran, 352, 264, 44, W);
-		*facile = TTF_RenderUTF8_Blended(police_petite, "Facile", couleur_texte_G); 
-		*normal = TTF_RenderUTF8_Blended(police_petite, "Normal", couleur_texte_W); 
-		*expert = TTF_RenderUTF8_Blended(police_petite, "Expert", couleur_texte_G); 
+		facile = TTF_RenderUTF8_Blended(police_petite, "Facile", couleur_texte_G); 
+		normal = TTF_RenderUTF8_Blended(police_petite, "Normal", couleur_texte_W); 
+		expert = TTF_RenderUTF8_Blended(police_petite, "Expert", couleur_texte_G); 
 	}
 	if (niveau == 3)	// niveau expert
 	{
@@ -37,36 +38,43 @@ void display_niveau(int niveau, TTF_Font *police_petite, SDL_Surface *ecran, SDL
 		drawSquare(ecran, 220, 264, 44, W);
 		drawSquare(ecran, 308, 264, 44, G);
 		drawSquare(ecran, 352, 264, 44, G);
-		*facile = TTF_RenderUTF8_Blended(police_petite, "Facile", couleur_texte_G); 
-		*normal = TTF_RenderUTF8_Blended(police_petite, "Normal", couleur_texte_G); 
-		*expert = TTF_RenderUTF8_Blended(police_petite, "Expert", couleur_texte_W); 
+		facile = TTF_RenderUTF8_Blended(police_petite, "Facile", couleur_texte_G); 
+		normal = TTF_RenderUTF8_Blended(police_petite, "Normal", couleur_texte_G); 
+		expert = TTF_RenderUTF8_Blended(police_petite, "Expert", couleur_texte_W); 
 	}
+	drawTexture(ecran, 62, 274, facile);
+	drawTexture(ecran, 184, 274, normal);
+	drawTexture(ecran, 326, 274, expert);
+	SDL_FreeSurface(facile);
+	SDL_FreeSurface(normal);
+	SDL_FreeSurface(expert);
 }
 
 
 SDL_Surface *menu(TTF_Font *police_petite, TTF_Font *police_moyenne, TTF_Font *police_grande, int *size, int *difficulte, int *nbr_coups_max)
 {
-	SDL_Surface *ecran, *nom_jeu, *taille_jeu, *niveau_jeu, *facile, *normal, *expert, *icone_plus, *icone_moins, *icone_jouer;
+	SDL_Surface *ecran, *nom_jeu, *taille_jeu, *niveau_jeu, *icone_plus, *icone_moins, *icone_jouer;
 	SDL_Event event;
 	SDL_Color couleur_texte_W = {255, 255, 255, 42}, couleur_texte_G = {51, 51, 51, 42};
 
-	bool flip = true;
 	int continuer = 1, compteur = 3, niveau = 2, size_window = 440, background_size = 10, i = 0;
+	bool flip = true;
 	char compteur_txt[50];
 
 	ecran = SDL_SetVideoMode(size_window, size_window, 32, SDL_HWSURFACE);
 
 	nom_jeu = TTF_RenderUTF8_Blended(police_grande, "ColorFlood", couleur_texte_G);
 	niveau_jeu = TTF_RenderUTF8_Blended(police_moyenne, "Niveau :", couleur_texte_G);
-
 	icone_plus = SDL_LoadBMP("img/plus.bmp");
 	icone_moins = SDL_LoadBMP("img/moins.bmp");
 	icone_jouer = SDL_LoadBMP("img/jouer.bmp");
 
+	// gestion arrière-plan "jeu" du menu
 	int time_between_moves = 875;
-	char* chemin = malloc(100*sizeof(char)); //problème sur Valgrind
+	unsigned long time = SDL_GetTicks();
+	unsigned long time_next_move = time + time_between_moves;
 	grille plateau = random_grille(background_size), plateau_sol = copie(plateau, background_size);
-	chemin = solution_rapide(plateau_sol, background_size, nbr_coups_max);
+	char *chemin = solution_rapide(plateau_sol, background_size, nbr_coups_max);
 	free_space(plateau_sol, background_size);
 	
 	// gestion du son
@@ -75,8 +83,6 @@ SDL_Surface *menu(TTF_Font *police_petite, TTF_Font *police_moyenne, TTF_Font *p
 	Mix_VolumeMusic(30);
 	Mix_PlayMusic(background_music, -1);
 
-	unsigned long time = SDL_GetTicks();
-	unsigned long time_next_move = time + time_between_moves;
 	while (continuer)
 	{
 		while (SDL_PollEvent(&event))
@@ -131,7 +137,7 @@ SDL_Surface *menu(TTF_Font *police_petite, TTF_Font *police_moyenne, TTF_Font *p
 				}
 			}
 		}
-		if (time > time_next_move)
+		if (time > time_next_move) // gestion arrière-plan "jeu" du menu
 		{
 			if (if_flood(plateau, background_size) != 1)
 			{
@@ -152,30 +158,6 @@ SDL_Surface *menu(TTF_Font *police_petite, TTF_Font *police_moyenne, TTF_Font *p
 				free_space(plateau_sol, background_size);
 			}
 		}
-
-		if (flip)
-		{
-			flip = false;
-			display_plateau(ecran, plateau, background_size, size_window, 0, 0);
-			sprintf(compteur_txt, "Taille : %2d", compteur);
-			taille_jeu = TTF_RenderUTF8_Blended(police_moyenne, compteur_txt, couleur_texte_G);
-			display_niveau(niveau, police_petite, ecran, &facile, &normal, &expert, couleur_texte_W, couleur_texte_G);
-			drawTexture(ecran, 41, 5, nom_jeu);
-			drawTexture(ecran, 78, 100, taille_jeu);
-			drawTexture(ecran, 125, 195, niveau_jeu);
-			drawTexture(ecran, 62, 274, facile);
-			drawTexture(ecran, 184, 274, normal);
-			drawTexture(ecran, 326, 274, expert);
-			drawTexture(ecran, 308, 88, icone_plus);
-			drawTexture(ecran, 308, 132, icone_moins);
-			drawTexture(ecran, 176, 352, icone_jouer);
-			SDL_Flip(ecran);
-			SDL_FreeSurface(taille_jeu);
-			SDL_FreeSurface(facile);
-			SDL_FreeSurface(normal);
-			SDL_FreeSurface(expert);
-		}
-
 		unsigned long new_time = SDL_GetTicks();
 		unsigned long elapsed_time = new_time - time;
 		if(elapsed_time < 1000/60)
@@ -183,13 +165,30 @@ SDL_Surface *menu(TTF_Font *police_petite, TTF_Font *police_moyenne, TTF_Font *p
 			SDL_Delay(1000/60 - elapsed_time);
 		}
 		time = new_time;
+
+		if (flip)
+		{
+			flip = false;
+			display_plateau(ecran, plateau, background_size, size_window, 0, 0);
+			sprintf(compteur_txt, "Taille : %2d", compteur);
+			taille_jeu = TTF_RenderUTF8_Blended(police_moyenne, compteur_txt, couleur_texte_G);
+			drawTexture(ecran, 41, 5, nom_jeu);
+			drawTexture(ecran, 78, 100, taille_jeu);
+			drawTexture(ecran, 308, 88, icone_plus);
+			drawTexture(ecran, 308, 132, icone_moins);
+			drawTexture(ecran, 125, 195, niveau_jeu);
+			drawTexture(ecran, 176, 352, icone_jouer);
+			display_niveau(niveau, police_petite, ecran, couleur_texte_W, couleur_texte_G);
+			SDL_Flip(ecran);
+			SDL_FreeSurface(taille_jeu);
+		}
 	}
 	free_space(plateau, background_size);
 	free(chemin);
 	SDL_FreeSurface(nom_jeu);
-	SDL_FreeSurface(niveau_jeu);
 	SDL_FreeSurface(icone_plus);
 	SDL_FreeSurface(icone_moins);
+	SDL_FreeSurface(niveau_jeu);
 	SDL_FreeSurface(icone_jouer);
 	Mix_FreeMusic(background_music);
 
--- menu.h
+++ menu.h
@@ -19,20 +19,19 @@
 
 
 /**
- * \fn void display_niveau(int niveau, SDL_Surface *ecran, SDL_Surface **facile, SDL_Surface **normal, SDL_Surface **expert, SDL_Color couleur_texte_W, SDL_Color couleur_texte_G)
+ * \fn void display_niveau(int niveau, TTF_Font *police_petite, SDL_Surface *ecran, SDL_Color couleur_texte_W, SDL_Color couleur_texte_G)
  * \brief Affichage du choix du niveau du jeu
  *
  * \param niveau Niveau du jeu choisi (1 pour facile, 2 pour normal, 3 pour expert)
  * \param police_petite Police du texte
  * \param ecran L'écran du menu
- * \param facile, normal, expert Pointeurs du texte correspondant
  * \param size Taille du jeu (grille size*size)
  * \param couleur_texte_W, couleur_texte_G La couleur du texte (W pour blanc, G pour gris)
  */
-void display_niveau(int niveau, TTF_Font *police_petite, SDL_Surface *ecran, SDL_Surface **facile, SDL_Surface **normal, SDL_Surface **expert, SDL_Color couleur_texte_W, SDL_Color couleur_texte_G);
+void display_niveau(int niveau, TTF_Font *police_petite, SDL_Surface *ecran, SDL_Color couleur_texte_W, SDL_Color couleur_texte_G);
 
 /**
- * \fn SDL_Surface *menu(TTF_Font *police_moyenne, TTF_Font *police_grande, int *size, int *difficulte, int *nbr_coups_max);
+ * \fn SDL_Surface *menu(TTF_Font *police_petite, TTF_Font *police_moyenne, TTF_Font *police_grande, int *size, int *difficulte, int *nbr_coups_max);
  * \brief Affiche un menu pour choisir la taille du jeu 
  *
  * \param police_petite, police_moyenne, police_grande Différentes polices du texte
